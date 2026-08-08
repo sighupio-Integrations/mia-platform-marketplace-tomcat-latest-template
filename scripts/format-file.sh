9f42e7fa1a26baac0170cdb48588caeb577f36ae
SIGHUP Engineering <engineering@sighup.io>
2026-08-08 03:01:18 +0000

    chore: update tomcat-latest template

---
+++ scripts/format-file.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# Copyright (c) 2017-present SIGHUP s.r.l All rights reserved.
+# Use of this source code is governed by a proprietary
+# license that can be found in the LICENSE file.
+
+set -e
+set -o errexit -o nounset
+
+IGNORE_PATHS='.git/,.github/,.vscode/,.idea/'
+
+file-cr --text --ignore "${IGNORE_PATHS}" --fix --path .
+file-crlf --text --ignore "${IGNORE_PATHS}" --fix --path .
+file-nullbyte --text --ignore "${IGNORE_PATHS}" --fix --path .
+file-trailing-newline --text --ignore "${IGNORE_PATHS}" --fix --path .
+file-trailing-single-newline --text --ignore "${IGNORE_PATHS}" --fix --path .
+file-trailing-space --text --ignore "${IGNORE_PATHS}" --fix --path .
+file-utf8 --text --ignore "${IGNORE_PATHS}" --fix --path .
+file-utf8-bom --text --ignore "${IGNORE_PATHS}" --fix --path .
---
+++ scripts/lint-yaml.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# Copyright (c) 2017-present SIGHUP s.r.l All rights reserved.
+# Use of this source code is governed by a proprietary
+# license that can be found in the LICENSE file.
+
+set -e
+set -o errexit -o nounset
+
+yamllint -c .rules/yamllint.yaml .
